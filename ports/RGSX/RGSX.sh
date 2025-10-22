4530ac3017e95683b35cd5b310edb5444170dfd4
skymike03 <mike.bosche@laposte.net>
2025-10-22 02:14:35 +0200

    v2.3.0.7 - minor updates to install script

--- ports/RGSX/RGSX.sh
+++ ports/RGSX/RGSX.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Supprimer SDL_VIDEODRIVER=fbcon pour laisser SDL choisir le pilote
-# export SDL_VIDEODRIVER=fbcon
-/usr/bin/python3 /userdata/roms/ports/RGSX
+#!/usr/bin/env python3
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+python3 "$SCRIPT_DIR/__main__.py"
